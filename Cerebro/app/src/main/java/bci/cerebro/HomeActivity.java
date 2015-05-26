ef09e6c650ca987ffabf553e903e3cbd81cf6b4b
Suryoday Basak <suryodaybasak@gmail.com>
2015-05-27 01:22:33 +0530

    Adding home screen

---
+++ Cerebro/app/src/main/java/bci/cerebro/HomeActivity.java
@@ -0,0 +1,15 @@
+package bci.cerebro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by suryo on 27/5/15.
+ */
+public class HomeActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+}
